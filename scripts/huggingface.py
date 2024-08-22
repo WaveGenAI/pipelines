48b0e196eeef5abc2ebeaa8cdb0f0c45da744d98
Jourdelune <jourdelune863@gmail.com>
2024-08-22 20:30:41 +0200

    improve pipelines

--- scripts/huggingface.py
+++ scripts/huggingface.py
@@ -15,22 +15,25 @@ logging.basicConfig(
     handlers=[logging.StreamHandler()],
 )
 
-# Remove invalid audio files
-for audio_file in os.listdir(args.folder):
-    if not audio_file.endswith(".mp3"):
-        continue
 
-    path = os.path.join(args.folder, audio_file)
-    file = eyed3.load(path)
+def filter_audio_files(folder):
+    # Remove invalid audio files
+    for audio_file in os.listdir(folder):
+        if not audio_file.endswith(".mp3"):
+            continue
 
-    if file is None:
-        logging.warning("Error processing audio file %s", audio_file)
-        os.remove(os.path.join(args.folder, audio_file))
+        path = os.path.join(args.folder, audio_file)
+        file = eyed3.load(path)
 
-dataset = load_dataset("audiofolder", data_dir=args.folder)
+        if file is None or file.info is None or file.info.time_secs < 1:
+            logging.warning("Error processing audio file %s", audio_file)
+            os.remove(path)
+
+
+filter_audio_files(args.folder)
+
 dataset = load_dataset("audiofolder", data_dir=args.folder).cast_column(
     "audio", Audio(mono=False, sampling_rate=44100)
 )
 
-# push the dataset to hub
 dataset.push_to_hub("WaveGenAI/audios")
